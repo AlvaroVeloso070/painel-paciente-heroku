5c7ad8f1173aa4a0eccc28ef7210f98e7eaa2ba0
AlvaroVeloso070 <AlvaroVeloso070@users.noreply.github.com>
2022-09-12 04:37:37 +0000

    deploy: cf1a0169f846f4cce1a331417542a20c9ba27f0e

---
+++ precache-manifest.ef6921ecad4dc1c82e403f7f11125e46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "543719619b4577150fbe",
+    "url": "/static/css/main.a95a57e1.chunk.css"
+  },
+  {
+    "revision": "543719619b4577150fbe",
+    "url": "/static/js/main.54371961.chunk.js"
+  },
+  {
+    "revision": "ce7af43862a79df7522c",
+    "url": "/static/js/1.ce7af438.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a935c72439c855340a8bc90270e0fba7",
+    "url": "/static/media/medicine-animate.a935c724.svg"
+  },
+  {
+    "revision": "efe9e543e7db170d44667447d1ca12d7",
+    "url": "/static/media/profile-data-animate.efe9e543.svg"
+  },
+  {
+    "revision": "4109b956f698ee643a0830b11bb82fc0",
+    "url": "/index.html"
+  }
+];
